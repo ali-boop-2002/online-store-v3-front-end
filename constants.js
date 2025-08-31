1aca44d343e701c0ee15c9cb1182e2041eabe79f
Ali Shahid <ali@MacBookAir.lan>
2025-08-30 22:04:21 -0400

    imprve cors

--- constants.js
+++ constants.js
@@ -1,9 +1,9 @@
 export const API_ENDPOINTS = {
-  BASE: import.meta.env.VITE_BASE_URL,
-  PRODUCTS: import.meta.env.VITE_PRODUCTS_URL,
-  USERS: import.meta.env.VITE_USERS_URL,
-  PAYMENT: import.meta.env.VITE_PAYMENT_URL,
-  UPLOAD: import.meta.env.VITE_UPLOAD_URL,
-  ORDERS: import.meta.env.VITE_ORDER_URL,
-  REVIEWS: import.meta.env.VITE_REVIEW_URL,
+  BASE: "https://online-store-v3-a3b15e62d3a8.herokuapp.com",
+  PRODUCTS: "/api/product",
+  USERS: "/api/users",
+  PAYMENT: "/api/payment",
+  UPLOAD: "/api/upload",
+  ORDERS: "/api/orders",
+  REVIEWS: "/api/review",
 };
